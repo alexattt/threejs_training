119b184348780623f6751f6d4a83dc76adc3313e
AleksandraT99 <49170656+AleksandraT99@users.noreply.github.com>
2020-07-21 00:17:36 +0300

    finally three.js setup ready

--- main.js
+++ main.js
@@ -1,21 +1,54 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+console.log(window.THREE);
 
-var renderer = new THREE.WebGLRenderer();
+var camera = new THREE.PerspectiveCamera(
+    55,                                   
+    window.innerWidth / window.innerHeight,
+    0.1,                                  
+    1000                                 
+);
+
+camera.position.set(80, 80, 80);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xfff6e6);
+
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+renderer.render(scene, camera);
 
-camera.position.z = 5;
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', function () { renderer.render(scene, camera); });
 
-function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-animate();
+var geometry = new THREE.OctahedronGeometry(10, 1);
+var material = new THREE.MeshStandardMaterial({
+    color: 0xff0051,
+    shading: THREE.FlatShading, // default is THREE.SmoothShading
+    metalness: 0,
+    roughness: 1
+});
+var shapeOne = new THREE.Mesh(geometry, material);
+shapeOne.position.y += 10;
+scene.add(shapeOne);
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(ambientLight);
+
+var pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(25, 50, 25);
+scene.add(pointLight);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+pointLight.castShadow = true;
+pointLight.shadow.mapSize.width = 1024;
+pointLight.shadow.mapSize.height = 1024;
+
+shapeOne.castShadow = true;
+shapeOne.receiveShadow = true;
+
+var shadowMaterial = new THREE.ShadowMaterial({ color: 0xeeeeee });
+shadowMaterial.opacity = 0.5;
